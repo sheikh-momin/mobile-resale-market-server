cdb74fcd016a571e9f0cd9f3bc171b1cb788095b
sheikh-momin <momin.emon14@gmail.com>
2022-11-29 10:37:05 -0800

    get users sellers by email

--- index.js
+++ index.js
@@ -176,6 +176,13 @@ async function run(){
         const user = await usersCollection.findOne(query);
         res.send({ isAdmin: user?.role === 'admin' });
       });
+
+      app.get('/users/seller/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isSeller: user?.accountType === 'Seller' });
+      });
     }
     finally{}
 }
