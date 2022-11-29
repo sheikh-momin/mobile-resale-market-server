31e770875e29f5946fbd6f686ae148de3923027b
sheikh-momin <momin.emon14@gmail.com>
2022-11-29 10:36:29 -0800

    get users admin by email

--- index.js
+++ index.js
@@ -169,6 +169,13 @@ async function run(){
         const seller = await usersCollection.find(query).toArray();
         res.send(seller);
       });
+
+      app.get('/users/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isAdmin: user?.role === 'admin' });
+      });
     }
     finally{}
 }
