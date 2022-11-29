5488dd4375840988bbc8066d6354c3107619b254
sheikh-momin <momin.emon14@gmail.com>
2022-11-29 09:33:15 -0800

    add access token and stripe secret

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v3avrd5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
