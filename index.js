de2a30fee2f71f2fbe7b71411ca20e810959eae8
sheikh-momin <momin.emon14@gmail.com>
2022-11-29 10:32:33 -0800

    delete products

--- index.js
+++ index.js
@@ -102,6 +102,13 @@ async function run(){
         const result = await allPhoneCollection.updateOne(filter, updatedDoc, options);
         res.send(result);
       });
+
+      app.delete('/products/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await allPhoneCollection.deleteOne(query);
+        res.send(result);
+      });
     }
     finally{}
 }
