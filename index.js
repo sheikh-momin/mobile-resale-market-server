21e5ec20f3809cb8bfc0b49266d2da86c05ab36b
sheikh-momin <momin.emon14@gmail.com>
2022-11-29 10:22:07 -0800

    add function for varifay jwt

--- index.js
+++ index.js
@@ -16,3 +16,30 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v3avrd5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('phone resale house server running')
+});
+
+app.listen(port, () => {
+  console.log(`phone resale house ${port}`);
+});
+
